3d5da6d90a14bb272e8159fbfa4a381ebcffc400
SpikyAtLinux <spikyatlinux@gmail.com>
2026-08-10 16:24:47 +0200

    Updated to v2.2

--- completion/pwrfzf_completion.sh
+++ completion/pwrfzf_completion.sh
@@ -7,21 +7,22 @@ _pwrfzf() {
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
-  opts="-h --help -k --keybindings -V --version"
+  opts="-h --help --help-full -k --keybindings -V --version -c --config --depclean --preserved-rebuild --sync"
 
   case "${prev}" in
-    -h|--help|-k|--keybindings|-V|--version)
+    -h|--help|--help-full|-k|--keybindings|-V|--version|--depclean|--preserved-rebuild|--sync)
       return 0
       ;;
   esac
 
+  # Autocomplete die Argumente/Flags
   if [[ "${cur}" == -* ]]; then
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     return 0
   fi
 
+  # Fallback auf Standard-Datei/Ordner-Autovervollständigung für Query-Eingaben
   COMPREPLY=( $(compgen -f -- "${cur}") )
 }
 
 complete -F _pwrfzf pwrfzf
-
